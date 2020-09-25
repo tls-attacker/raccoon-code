fe09813a8ad28edca6c5159dc7ae2f854f2483ed
Nimrod Aviram <nimrod.aviram@gmail.com>
2020-09-25 13:49:06 +0300

    Faster script.

--- figure_3/go.sh
+++ figure_3/go.sh
@@ -3,9 +3,9 @@
 gcc -std=c11 openssl_hmac.c -lssl -lcrypto -ldl -o openssl_hmac || exit 1
 
 # Change the number of measurements per key length to 10k to reproduce our
-# setup, but this will take time. Or just use 1k.
-./openssl_hmac sha256 1000 > raw_sha256
-./openssl_hmac sha384 1000 > raw_sha384
+# setup, but this will take time. Or just use 1k as a good compromise.
+./openssl_hmac sha256 100 > raw_sha256
+./openssl_hmac sha384 100 > raw_sha384
 
 ./get_medians.py raw_sha256 > medians_sha256
 ./get_medians.py raw_sha384 > medians_sha384
